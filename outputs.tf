a41d20af762501159118e2184d7652363b265304
Craig Thacker <craigthackerx@gmail.com>
2024-02-19 23:31:24 +0000

    Update code

--- outputs.tf
+++ outputs.tf
@@ -8,11 +8,14 @@ output "event_subscription_names" {
   value       = [for sub in azurerm_eventgrid_system_topic_event_subscription.event_subscription : sub.name]
 }
 
+output "event_subscription_rg_names" {
+  description = "The resource group names of the Event Grid System Topic Event Subscriptions."
+  value = [
+    for sub in azurerm_eventgrid_system_topic_event_subscription.event_subscription : sub.resource_group_name
+  ]
+}
+
 output "event_subscription_topic_names" {
   description = "The names of the System Topics associated with the Event Subscriptions."
   value       = [for sub in azurerm_eventgrid_system_topic_event_subscription.event_subscription : sub.system_topic]
 }
-
-output "event_subscription_rg_names" {
-  description = "The resource group names of the Event Grid System Topic Event Subscriptions."
-  value       = [for sub in azurerm_eventgrid_system_topic_event_subscription.event_subscription : sub.resource_group_name}
